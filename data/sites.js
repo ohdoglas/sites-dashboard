48650c98b6b67615b00ab2d5140d6f88395fad5c
Douglas <dughlaisdd@gmail.com>
2026-05-11 22:58:11 -0300

    new sites added

--- data/sites.js
+++ data/sites.js
@@ -6,7 +6,6 @@ window.TEACHER_USB_SITES = {
       items: [
         { title: 'ChatGPT', description: 'IA e produtividade', href: 'https://chat.openai.com', image: 'https://chat.openai.com/favicon.ico' },
         { title: 'GitHub', description: 'Código e repositórios', href: 'https://github.com', image: 'https://github.githubassets.com/favicons/favicon.svg' },
-        { title: 'Tinkercad', description: 'Simulação e prototipagem', href: 'https://www.tinkercad.com', image: 'https://www.tinkercad.com/favicon.ico' },
         { title: 'Trello', description: 'Organização e Produtividade', href: 'https://trello.com', image: 'https://trello.com/favicon.ico' },
         { title: 'Server Defense CLI', description: 'Simulador de CLI', href: 'https://ohdoglas.github.io/server-defense-cli/', image: 'https://content.imageresizer.com/images/memes/Hacker-Doge-meme-9.jpg' },
         { title: 'Gestão Adm Empresas', description: 'Simulador de Gestão de Empresas', href: 'https://ohdoglas.github.io/gestao_admin_dashboard_entrega05_github_pages_acesso/index.html', image: 'https://cdn-icons-png.flaticon.com/512/6512/6512792.png' }
@@ -19,16 +18,32 @@ window.TEACHER_USB_SITES = {
       ]
     },
     {
-      id: 'desenvolvimento e IA',
-      title: 'Desenvolvimento e IA',
+      id: 'robotica-eletronica',
+      title: 'Robótica e Eletrônica',
+      items: [
+        //https://wokwi.com/
+        {title: 'Tinkercad', description: 'Simulação e prototipagem', href: 'https://www.tinkercad.com', image: 'https://www.tinkercad.com/favicon.ico' },
+        {title: 'Wokwi', description: 'Robótica e Eletrônica', href: 'https://wokwi.com/', image:'https://br.mouser.com/images/suppliers/wokwi.png'},
+        {title: 'Circuit Labs', description: 'Robótica e Eletrônica', href: 'https://www.circuitlab.com/', image:'https://www.circuitlab.com/assets/images/og_logo.png'},
+      ]
+    },
+    {
+      id: 'IA',
+      title: 'IA',
+      items: [
+        { title: 'ChatGPT', description: 'IA e produtividade', href: 'https://chat.openai.com', image: 'https://chat.openai.com/favicon.ico' },
+        { title: 'Claude', description: 'IA e produtividade', href: 'https://claude.ai/login', image: 'https://raw.githubusercontent.com/lobehub/lobe-icons/refs/heads/master/packages/static-png/dark/claude-color.png' },
+        { title: 'Gemini', description: 'IA e produtividade', href: 'https://gemini.google.com/app?hl=pt-BR', image: 'https://www.google.com/favicon.ico' },
+        { title: 'Hugging Face', description: 'IA e produtividade', href: 'https://huggingface.co/', image: 'https://huggingface.co/favicon.ico' },
+      ]
+    },
+        {
+      id: 'desenvolvimento',
+      title: 'Desenvolvimento',
       items: [
         { title: 'GitHub', description: 'Código e versionamento', href: 'https://github.com', image: 'https://github.githubassets.com/favicons/favicon.svg' },
         { title: 'VS Code', description: 'Editor de código', href: 'https://vscode.dev', image: 'https://code.visualstudio.com/favicon.ico' },
         { title: 'Python Online', description: 'Editor de código', href: 'https://www.online-python.com/', image: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png' },
-        { title: 'Claude', description: 'IA e produtividade', href: 'https://claude.ai/login', image: 'https://raw.githubusercontent.com/lobehub/lobe-icons/refs/heads/master/packages/static-png/dark/claude-color.png' },
-        { title: 'ChatGPT', description: 'IA e produtividade', href: 'https://chat.openai.com', image: 'https://chat.openai.com/favicon.ico' },
-        { title: 'Gemini', description: 'IA e produtividade', href: 'https://gemini.google.com/app?hl=pt-BR', image: 'https://www.google.com/favicon.ico' },
-        { title: 'Hugging Face', description: 'IA e produtividade', href: 'https://huggingface.co/', image: 'https://huggingface.co/favicon.ico' },
         { title: 'Replit', description: 'Editor de código online', href: 'https://replit.com/', image: 'https://img.icons8.com/color/1200/replit.jpg' },
         { title: 'CodePen', description: 'Editor de código online', href: 'https://codepen.io/', image: 'https://codepen.io/favicon.ico' },
       ]
@@ -39,13 +54,6 @@ window.TEACHER_USB_SITES = {
       items: [
         { title: 'Canva', description: 'Design e apresentações', href: 'https://www.canva.com', image: 'https://static.canva.com/static/images/favicon.ico' },
         { title: 'Figma', description: 'Design e apresentações', href: 'https://www.figma.com/pt-br/', image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg' }
-      ]
-    },
-    {
-      id: 'robotica-eletronica',
-      title: 'Robótica e Eletrônica',
-      items: [
-
       ]
     },
         {
