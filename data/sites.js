9eff98781ae76df4818bec19338de9e6edb948a7
Douglas <dughlaisdd@gmail.com>
2026-06-04 17:44:46 -0300

    new sites added

--- data/sites.js
+++ data/sites.js
@@ -35,6 +35,16 @@ window.TEACHER_USB_SITES = {
         { title: 'Claude', description: 'IA e produtividade', href: 'https://claude.ai/login', image: 'https://raw.githubusercontent.com/lobehub/lobe-icons/refs/heads/master/packages/static-png/dark/claude-color.png' },
         { title: 'Gemini', description: 'IA e produtividade', href: 'https://gemini.google.com/app?hl=pt-BR', image: 'https://www.google.com/favicon.ico' },
         { title: 'Hugging Face', description: 'IA e produtividade', href: 'https://huggingface.co/', image: 'https://huggingface.co/favicon.ico' },
+        { title: 'GeeLark', description: 'Celulares virtuais e IA', href: 'https://www.geelark.com/', image: 'https://www.geelark.com/wp-content/uploads/2024/03/%E9%BB%91%E5%AD%97-300x61.png'},
+        { title: 'AI Studio Google', description: 'Geração de aplicativos', href: 'https://aistudio.google.com/', image: 'https://miro.medium.com/v2/resize:fit:1400/0*izKi8_M2fKedl5fA'},
+        { title: 'Antigravity', description: 'Next gen agent', href: 'https://antigravity.google/', image: 'https://antigravity.google/assets/image/brand/antigravity-icon__full-color.png'},
+        { title: 'Stitch', description: 'Criação de prototipos figma-like', href: 'https://stitch.withgoogle.com/', image: 'https://miro.medium.com/1*6x04bXVqveDp_yZzAs22fQ.png'},
+        { title: 'Google Flow', description: 'Gere imagens e vídeos', href: 'https://labs.google/fx/tools/flow', image: 'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Flow_Sizzle_Thumbnail_1280_x_720_.width-700.format-webp.webp'},
+        { title: 'Z.AI', description: 'IA Chinesa', href: 'https://chat.z.ai/', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdQtM8BJDfLEhWW3D9VfgYB_f8ayfh9816Nw&s'},
+        { title: 'Qween3.6-Plus', description: 'IA Chinesa', href: 'https://chat.qwen.ai/', image: 'https://d7umqicpi7263.cloudfront.net/img/product/6a9a069b-87bb-479e-9f3d-1ea847f39db4.com/462fd004b16f152a30c8eb25481b0dc2'},
+        { title: 'KIMI.ai', description: 'IA Chinesa', href: 'https://www.kimi.com/', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrC6cAlvFjySE6YHnaZwbHZq8U7VkAvalQEw&s' },
+        { title: 'STEPFUN', description: 'IA Chinesa', href: 'https://chat.stepfun.com/chats/new', image: 'https://huggingface.co/stepfun-ai/step3/resolve/b85635bedaa20e14a1422f8bbc413c887d84226c/stepfun-logo.png'}
+
       ]
     },
         {
@@ -46,6 +56,8 @@ window.TEACHER_USB_SITES = {
         { title: 'Python Online', description: 'Editor de código', href: 'https://www.online-python.com/', image: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png' },
         { title: 'Replit', description: 'Editor de código online', href: 'https://replit.com/', image: 'https://img.icons8.com/color/1200/replit.jpg' },
         { title: 'CodePen', description: 'Editor de código online', href: 'https://codepen.io/', image: 'https://codepen.io/favicon.ico' },
+        { title: 'Coddy Tech', description: 'Aprendar a codar online', href: 'https://coddy.tech', image: 'https://coddy.tech/images/bit-start.svg'},
+        { title: 'React Bits', description: 'Componentes de React', href: 'https://reactbits.dev/', image: 'https://reactbits.dev/assets/react-bits-sticker-DuQtTs-F.png'}
       ]
     },
     {
@@ -53,7 +65,10 @@ window.TEACHER_USB_SITES = {
       title: 'Design',
       items: [
         { title: 'Canva', description: 'Design e apresentações', href: 'https://www.canva.com', image: 'https://static.canva.com/static/images/favicon.ico' },
-        { title: 'Figma', description: 'Design e apresentações', href: 'https://www.figma.com/pt-br/', image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg' }
+        { title: 'Figma', description: 'Design e Prototipagem de Apps', href: 'https://www.figma.com/pt-br/', image: 'https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg' },
+        { title: 'ASC11', description: 'Arte com ASC11', href: 'https://asc11.com/', image: 'https://upload.wikimedia.org/wikipedia/commons/4/45/BB-ASCII-art-screenshot-zebra.png'},
+        { title: 'LOPAKA.APP', description: 'GUI Display Design', href: 'https://lopaka.app/', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpD9mJhgAjr6GC3LPqnBpyGv6P0AzhYhxIGg&s'},
+        { title: 'SETTEI', description: 'Artbooks e Storyboards', href: 'https://setteidreams.net/settei/', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFZNw1iBXr9-Smb20_EUpSJcVxd1xtRgGf8A&s' }
       ]
     },
         {
